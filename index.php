cf4ce0d2d6aa8175c53520c8f75a55bed6c97f39
Advaith P. Pillai <apillai88@gatech.edu>
2025-02-22 08:11:15 -0500

    Set up index page and added Insurance Plan Matcher Form.

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+	<!--Metadata-->
+	<head>
+  
+		<!--Title-->
+		<title>Linksure | Your path to an insured life.</title>
+		
+	<!--CSS-->
+		
+		<!--Bootstrap Stylesheet-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+		
+		<!--Custon Stylesheet-->
+		<link rel="stylesheet" type="text/css" href="css/index.css" />
+  
+	</head>
+	
+	<!--Webpage Body-->
+	<body>
+	
+		<!--Navigation Bar-->
+		<?php include("navigation_bar.php");?>
+		
+		
+		<!--Main Jumbotron-->
+		<div id="jumbotron" class="p-5 bg-primary text-white">
+		
+			<!--Title-->
+			<h1 class="font-weight-bolder">Linksure</h1>
+					
+			<!--Search Bar-->
+			<div class="mb-3">
+					
+				<!--Input Box-->
+				<input type="text" id="search" class="form-control" placeholder="Type a guide name...">
+						
+				<!--Button-->
+				<button id="search-btn" class="btn btn-primary mt-3">Search</button>
+						
+		</div>
+
+		</script>
+	
+	</body>
+  
+</html>
---
+++ navigation_bar.php
@@ -0,0 +1,37 @@
+<!--Stylesheet-->
+<style>
+
+    /*Overrides Bootstrap styles for the Navigation Bar*/
+    .nav-link {
+
+        color: white !important;
+    }
+
+</style>
+
+
+<!--Create Navigation Bar-->
+<nav class="navbar navbar-dark bg-dark">
+
+	<!--Navigation Bar Container-->
+  <div class="container-fluid">
+  
+	<!--Title Button-->
+    <a class="navbar-brand" href="index.php">Linksure</a>
+
+    <!--Create list of Navigation Bar Elements-->
+    <ul class="nav ms-auto" style="color: white;" id="navbarlist" > <!-- ms-auto aligns the list to the right -->
+	
+		<!--Submit a Study Guide Button-->
+		<li class="nav-item">
+			
+			<!--Button-->
+			<a class="nav-link" href="insurance_matcher_form.php">Insurance Plan Matcher</a>
+			
+		</li>
+
+    </ul>
+	
+  </div>
+  
+</nav>
